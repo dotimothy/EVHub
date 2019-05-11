31fd6202a525ce0cfe03b38716d5c2de3629b275
bent1n <bennett.nguyen0@gmail.com>
2019-05-11 09:18:11 -0700

    Bottom nav fixed

--- app/src/main/java/com/example/evhub/MainActivity.java
+++ app/src/main/java/com/example/evhub/MainActivity.java
@@ -8,53 +8,51 @@ import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
-    final Fragment fragment1 = new Home();
-    final Fragment fragment2 = new Social();
-    final Fragment fragment3 = new Schoolloop();
-    final Fragment fragment4 = new Notifications();
-    final FragmentManager fm = getSupportFragmentManager();
-    Fragment active = fragment1;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    fm.beginTransaction().hide(active).show(fragment1).commit();
-                    active = fragment1;
-                    return true;
-                case R.id.navigation_social:
-                    fm.beginTransaction().hide(active).show(fragment2).commit();
-                    active = fragment2;
-                    return true;
-                case R.id.navigation_schoolloop:
-                    fm.beginTransaction().hide(active).show(fragment3).commit();
-                    active = fragment3;
-                    return true;
-                case R.id.navigation_notifications:
-                    fm.beginTransaction().hide(active).show(fragment4).commit();
-                    active = fragment4;
-                    return true;
-            }
-            return false;
-        }
-    };
-
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //loading the default fragment
+        loadFragment(new Home());
+        //getting bottom navigation view and attaching the listener
         BottomNavigationView navigation = findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        fm.beginTransaction().add(R.id.container, fragment4, "4").hide(fragment3).commit();
-        fm.beginTransaction().add(R.id.container, fragment3, "3").hide(fragment3).commit();
-        fm.beginTransaction().add(R.id.container, fragment2, "2").hide(fragment2).commit();
-        fm.beginTransaction().add(R.id.container, fragment1, "1").commit();
+        navigation.setOnNavigationItemSelectedListener(this);
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                fragment = new Home();
+                break;
+
+            case R.id.navigation_social:
+                fragment = new Social();
+                break;
+
+            case R.id.navigation_schoolloop:
+                fragment = new Schoolloop();
+                break;
+
+            case R.id.navigation_notifications:
+                fragment = new Notifications();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
 }
