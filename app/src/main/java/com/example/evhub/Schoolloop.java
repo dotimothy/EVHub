d6ae46bd3e938f083e0973883ae6e34c885a67bf
Timothy Do <47342411+TimTheCoder123@users.noreply.github.com>
2019-05-12 10:07:04 -0700

    Added Ben's Code

--- app/src/main/java/com/example/evhub/Schoolloop.java
+++ app/src/main/java/com/example/evhub/Schoolloop.java
@@ -1,35 +1,58 @@
 package com.example.evhub;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-/**
- * Created by Belal on 1/23/2018.
- */
-
 public class Schoolloop extends Fragment {
-
-    @Nullable
-    @Override
-
-
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_schoolloop, null);
-        WebView browser = (WebView) v.findViewById(R.id.schoolloop); //creating webview
-        browser.getSettings().setJavaScriptEnabled(true); //enables javascipt
-        browser.setWebViewClient(new WebViewClient()); //essential for opening websites in webview.
-        browser.loadUrl("https://evhs.schoolloop.com/portal/login"); //loading url for EVHS 
-        if(browser.canGoBack()) {
-            browser.goBack();
+    private WebView browser;
+    private View v;
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message message) {
+            switch (message.what) {
+                case 1: {
+                    webViewGoBack();
+                }
+                break;
+            }
         }
+    };
+    private void webViewGoBack(){
+        browser.goBack();
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.fragment_schoolloop, null);
+        browser = (WebView) v.findViewById(R.id.webView);
+        WebSettings settings = browser.getSettings();
+        settings.setSupportZoom(false);
+        settings.setJavaScriptEnabled(true);
+        browser.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        browser.getSettings().setBuiltInZoomControls(false);
+        browser.setWebViewClient(new WebViewClient()); //essential for opening websites in webview.
+        browser.loadUrl("https://evhs.schoolloop.com/portal/login");
+        browser.setOnKeyListener(new View.OnKeyListener(){
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_BACK
+                        && event.getAction() == MotionEvent.ACTION_UP
+                        && browser.canGoBack()) {
+                    handler.sendEmptyMessage(1);
+                    return true;
+                }
+                return false;
+            }
+        });
         return v;
     }
 }
