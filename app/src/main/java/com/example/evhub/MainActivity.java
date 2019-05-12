9710c29cd5df47bcd102bf38964109b9c9c33be3
Timothy Do <47342411+TimTheCoder123@users.noreply.github.com>
2019-05-12 08:21:50 -0700

    update

--- app/src/main/java/com/example/evhub/MainActivity.java
+++ app/src/main/java/com/example/evhub/MainActivity.java
@@ -4,17 +4,14 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //loading the default fragment
         loadFragment(new Home());
         //getting bottom navigation view and attaching the listener
         BottomNavigationView navigation = findViewById(R.id.navigation);
--- app/src/main/java/com/example/evhub/Schoolloop.java
+++ app/src/main/java/com/example/evhub/Schoolloop.java
@@ -3,22 +3,32 @@ package com.example.evhub;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 /**
  * Created by Belal on 1/23/2018.
  */
 
 public class Schoolloop extends Fragment {
+
     @Nullable
     @Override
+
+
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        //just change the fragment_dashboard
-        //with the fragment you want to inflate
-        //like if the class is HomeFragment it should have R.layout.home_fragment
-        //if it is DashboardFragment it should have R.layout.fragment_dashboard
-        return inflater.inflate(R.layout.fragment_schoolloop, null);
+        View v = inflater.inflate(R.layout.fragment_schoolloop, null);
+        WebView browser = (WebView) v.findViewById(R.id.webview);
+        browser.setWebViewClient(new WebViewClient()); //essential for opening websites in webview.
+        browser.loadUrl("https://evhs.schoolloop.com/portal/login");
+        if(browser.canGoBack()) {
+            browser.goBack();
+        }
+        return v;
     }
 }
