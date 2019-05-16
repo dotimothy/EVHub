566c03c8290df9188d3ab95c44b19dd29749b0e8
Bennett Nguyen <bennett.nguyen@gmail.com>
2019-05-15 19:41:38 -0700

    Update Home.java

--- app/src/main/java/com/example/evhub/Home.java
+++ app/src/main/java/com/example/evhub/Home.java
@@ -3,11 +3,9 @@ package com.example.evhub;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 public class Home extends Fragment {
     @Nullable
