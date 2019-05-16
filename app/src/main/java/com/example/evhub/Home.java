641184f1b6252fbbb442039177225c778aa3b546
ryan <yaoryan9044@EVB138S34.esuhsd.org>
2019-05-16 09:36:08 -0700

    cardview linked

--- app/src/main/java/com/example/evhub/Home.java
+++ app/src/main/java/com/example/evhub/Home.java
@@ -3,15 +3,36 @@ package com.example.evhub;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class Home extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_home, null);
+        CardView c = (CardView) v.findViewById(R.id.schedule);
+        CardView c1 = (CardView) v.findViewById(R.id.classes);
+        c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fr = getFragmentManager().beginTransaction();
+                fr.replace(R.id.fragment_container, new Schedule());
+                fr.commit();
+            }
+        });
+        c1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fr = getFragmentManager().beginTransaction();
+                fr.replace(R.id.fragment_container, new Classes());
+                fr.commit();
+            }
+        });
         return v;
     }
 }
