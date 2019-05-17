bec1adda6ff4502eb6c4199c4fbfd2c5066e2335
Timothy Do <47342411+TimTheCoder123@users.noreply.github.com>
2019-05-16 20:42:17 -0700

    added map but now work

--- app/src/main/java/com/example/evhub/Home.java
+++ app/src/main/java/com/example/evhub/Home.java
@@ -8,7 +8,6 @@ import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 public class Home extends Fragment {
     @Nullable
