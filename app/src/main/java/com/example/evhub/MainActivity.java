ba58c845318cc606b742e000046ca6905084cc1f
Bennett Nguyen <bennett.nguyen@gmail.com>
2019-05-17 20:05:12 -0700

    Update MainActivity.java

--- app/src/main/java/com/example/evhub/MainActivity.java
+++ app/src/main/java/com/example/evhub/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
@@ -47,6 +48,7 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         if (fragment != null) {
             getSupportFragmentManager()
                     .beginTransaction()
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
                     .addToBackStack(null)
                     .replace(R.id.fragment_container, fragment)
                     .commit();
@@ -54,7 +56,7 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         }
         return false;
     }
-    
+
     @Override
     public void onBackPressed(){
         FragmentManager fm = getFragmentManager();
