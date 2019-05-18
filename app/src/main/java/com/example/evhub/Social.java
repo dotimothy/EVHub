e6bcaa409aac9fd2c564baf488f97f52fe49b03e
Timothy Do <47342411+TimTheCoder123@users.noreply.github.com>
2019-05-18 12:29:27 -0700

    Addded the responses

--- app/src/main/java/com/example/evhub/Social.java
+++ app/src/main/java/com/example/evhub/Social.java
@@ -1,19 +1,74 @@
 package com.example.evhub;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.DownloadListener;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 
 public class Social extends Fragment {
-    @Nullable
+    private WebView browser;
+    private View v;
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message message) {
+            switch (message.what) {
+                case 1: {
+                    webViewGoBack();
+                }
+                break;
+            }
+        }
+    };
+
+    private void webViewGoBack(){
+        browser.goBack();
+    }
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_social, null);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.fragment_social, null);
+        browser = (WebView) v.findViewById(R.id.responses);
+        WebSettings settings = browser.getSettings();
+        settings.setSupportZoom(false);
+        settings.setJavaScriptEnabled(true);
+        browser.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        browser.getSettings().setBuiltInZoomControls(false);
+        browser.setWebViewClient(new WebViewClient());
+        browser.getSettings().setAppCacheEnabled(true);
+        browser.loadUrl("https://docs.google.com/spreadsheets/d/1KhwzHEI3jkgEMF0NIruOOFD9Lz58WoKACp6ggl5o_Fo/edit?ouid=115853807377850464165&usp=sheets_home&ths=true");
+        browser.setDownloadListener(new DownloadListener() {
+            public void onDownloadStart(String url, String userAgent,
+                                        String contentDisposition, String mimetype,
+                                        long contentLength) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+        browser.setOnKeyListener(new View.OnKeyListener(){
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_BACK
+                        && event.getAction() == MotionEvent.ACTION_UP
+                        && browser.canGoBack()) {
+                    handler.sendEmptyMessage(1);
+                    return true;
+                }
+                return false;
+            }
+        });
         Button b = (Button) v.findViewById(R.id.button);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
